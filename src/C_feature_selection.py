0576b28422f7b53e64a59e8489280fe86b6e7539
Priyanshu <priyanshu24003@gmail.com>
2026-02-10 17:57:49 +0530

    c3, B, C, D components added to the pipeline

--- src/C_feature_selection.py
+++ src/C_feature_selection.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 import logging
 import yaml
-
+import numpy as np
 
 
 # Ensure the "logs" directory exists
@@ -29,7 +29,22 @@ logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
 
-def check_column_scores(cols):
+def load_data(file_path: str) -> pd.DataFrame:
+    """Load data from a CSV file."""
+    try:
+        df = pd.read_csv(file_path)
+        df.fillna('', inplace=True)
+        logger.debug('Data loaded and NaNs filled from %s', file_path)
+        return df
+    except pd.errors.ParserError as e:
+        logger.error('Failed to parse the CSV file: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error occurred while loading the data: %s', e)
+        raise
+
+
+def check_column_scores(cols, df):
     """
     Trains and evaluates Logistic Regression via crossvalidation on the columns
     of the dataset with select indeces
@@ -46,24 +61,70 @@ def check_column_scores(cols):
     #Logistic Regression Model
     LR = LogisticRegression(max_iter = 5000)
     
-    return cross_val_score(LR, X_train[cols], y_train, cv = 5).mean()
+    df2 = df.copy()
+    y = df['Species']
+    X = df2.drop(["Species"], axis = 1)
 
-def FeatureSelection()
+    return cross_val_score(LR, X[cols], y, cv = 5).mean()
+
+def FeatureSelection(df):
+    try:
+        logger.debug('feature selection started ')
+        quals = ["Island", "Sex"]
+
+        quants = ["Culmen Length (mm)", "Culmen Depth (mm)",
+                "Flipper Length (mm)", "Body Mass (g)"]
+
+        combos = [[qual]+[quant1]+[quant2] for qual in quals for quant1 in quants for quant2 in quants if quant1 != quant2]
+
+        cv_scores = []
+        best_cv_score = -np.inf
+
+        for combo in combos:
+            score = check_column_scores(combo, df)
+            cv_scores.append(score)
+            
+            if cv_scores[-1] > best_cv_score:
+                best_cv_score = cv_scores[-1]
+                best_combo = combo
+
+        logger.debug("Best Feature selected which produces CV score: " + str(best_cv_score))
+
+        return best_combo
+    except:
+        logger.error('error occured while selecting best featueres')
+        raise
+    pass
+
+
+
+def save_data(df: pd.DataFrame, file_path: str) -> None:
+    """Save the dataframe to a CSV file."""
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        df.to_csv(file_path, index=False)
+        logger.debug('Data saved to %s', file_path)
+    except Exception as e:
+        logger.error('Unexpected error occurred while saving the data: %s', e)
+        raise
 
 
 def main():
     try:
 
-        params = load_params(params_path='params.yaml')
-        max_features = params['feature_engineering']['max_features']
+        # params = load_params(params_path='params.yaml')
+        # max_features = params['feature_engineering']['max_features']
 
         train_data = load_data('./data/interim/train_processed.csv')
         test_data = load_data('./data/interim/test_processed.csv')
 
-        # train_df, test_df = apply_tfidf(train_data, test_data, max_features)
+        BestFeatures = FeatureSelection(train_data) + ['Species']
 
-        save_data(train_df, os.path.join("./data", "processed", "train_tfidf.csv"))
-        save_data(test_df, os.path.join("./data", "processed", "test_tfidf.csv"))
+        train_df = train_data[BestFeatures]
+        test_df = test_data[BestFeatures]
+
+        save_data(train_df, os.path.join("./data", "processed", "train_final.csv"))
+        save_data(test_df, os.path.join("./data", "processed", "test_final.csv"))
     except Exception as e:
         logger.error('Failed to complete the feature engineering process: %s', e)
         print(f"Error: {e}")
---
+++ src/D_model_training.py
@@ -0,0 +1,145 @@
+import os
+import numpy as np
+import pandas as pd
+import pickle
+import logging
+import yaml
+from sklearn import svm
+from sklearn.model_selection import cross_val_score
+
+
+
+# Ensure the "logs" directory exists
+log_dir = 'logs'
+os.makedirs(log_dir, exist_ok=True)
+
+# logging configuration
+logger = logging.getLogger('D_Model_training')
+logger.setLevel('DEBUG')
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel('DEBUG')
+
+log_file_path = os.path.join(log_dir, 'D_Model_Training.log')
+file_handler = logging.FileHandler(log_file_path)
+file_handler.setLevel('DEBUG')
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
+
+
+def load_data(file_path: str) -> pd.DataFrame:
+    """
+    Load data from a CSV file.
+    
+    :param file_path: Path to the CSV file
+    :return: Loaded DataFrame
+    """
+    try:
+        df = pd.read_csv(file_path)
+        logger.debug('Data loaded from %s with shape %s', file_path, df.shape)
+        return df
+    except pd.errors.ParserError as e:
+        logger.error('Failed to parse the CSV file: %s', e)
+        raise
+    except FileNotFoundError as e:
+        logger.error('File not found: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error occurred while loading the data: %s', e)
+        raise
+
+
+
+def train_model(X_train: np.ndarray, y_train: np.ndarray,) -> svm.SVC:
+    """
+    Train the RandomForest model.
+    
+    :param X_train: Training features
+    :param y_train: Training labels
+    :param params: Dictionary of hyperparameters
+    :return: Trained RandomForestClassifier
+    """
+    try:                        
+        gammas = np.linspace(0.05, 5, 100)
+
+        scores = []
+        best_score = -np.inf
+
+        for g in gammas:
+            SVM = svm.SVC(gamma = g)
+
+            scores.append(cross_val_score(SVM, X_train, y_train, cv = 5).mean())
+            
+            if scores[-1] > best_score:
+                best_score = scores[-1]
+                best_gamma = g
+                
+        best_gamma, best_score
+
+        SVM = svm.SVC(gamma = best_gamma)
+
+        SVM.fit(X_train, y_train)
+        
+        logger.debug(f'model has been trained with score on training data :{SVM.score(X_train, y_train)} ')
+
+        return SVM
+
+    
+    except ValueError as e:
+        logger.error('ValueError during model training: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Error during model training: %s', e)
+        raise
+
+
+
+def save_model(model, file_path: str) -> None:
+    """
+    Save the trained model to a file.
+    
+    :param model: Trained model object
+    :param file_path: Path to save the model file
+    """
+    try:
+        # Ensure the directory exists
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        
+        with open(file_path, 'wb') as file:
+            pickle.dump(model, file)
+        logger.debug('Model saved to %s', file_path)
+    except FileNotFoundError as e:
+        logger.error('File path not found: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Error occurred while saving the model in the pickle file: %s', e)
+        raise
+
+
+
+def main():
+    try:
+
+        train_data = load_data('./data/processed/train_final.csv')
+        X_train = train_data.iloc[:, :-1].values
+        y_train = train_data.iloc[:, -1].values
+
+        clf = train_model(X_train, y_train,)
+        
+        model_save_path = 'models/model.pkl'
+        save_model(clf, model_save_path)
+
+    except Exception as e:
+        logger.error('Failed to complete the model building process: %s', e)
+        print(f"Error: {e}")
+
+
+if __name__ == '__main__':
+    main()
